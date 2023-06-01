e8dbaeab01985acfb0e99f21a225cb606c5533fc
Svetlana Golubeva <svetlana020491@gmail.com>
2023-06-01 14:05:09 +0500

    Готово

--- Program.cs
+++ Program.cs
@@ -24,29 +24,54 @@ void Task15()
         //обозначающую день недели, и проверяет, является ли этот день выходным.
 
 int number = InputNumber("Введите число, соответствующее дню недели");
-if  (number ==1 |number < 6) Console.WriteLine($"Этот день рабочий");
-else if (number == 6 || number == 7) Console.WriteLine($"Этот день выходной");
-else Console.WriteLine($"Данные введены не верно");
-
+if  (number ==1 |number < 6) 
+{
+        Console.WriteLine($"Этот день рабочий");
+        }
+else if (number == 6 || number == 7) 
+{
+        Console.WriteLine($"Этот день выходной");
+        }
+else 
+{       
+        Console.WriteLine($"Данные введены не верно");
+}
 }
 
-
+void Task13()
+{
 //Задача 13: Напишите программу, которая с помощью деления выводит третью цифру 
 //заданного числа или сообщает, что третьей цифры нет.
 
-// 78 -> третьей цифры нет
-// 3267432979 -> 6
-
-
 
 int number = InputNumber("Введите число");
-if (number > 999 ) 
- {while (number > 999 )
-  {  number = number /10;}  Console.WriteLine($"{number % 10}");} //если number > 999,
-  // надо number = number /10, и никак не получается
-else if (number < 99) Console.WriteLine($"в этом числе нет 3й цифры");
-// 3 семинар еще не смотрела 
+number = Math.Abs(number);
+if (number > 99) // если верно, шуруем в цикл
+{       
+        while(number > 1000) // если число больше 1000 
+        {
+                number = number / 10; // мы его тут уменьшаем
+        }      
+        Console.Write($"{number} -> ");
+        Console.WriteLine($"Третья цифра равно {number %10}");
+}
+else // если ИФ ложный
+{
+        Console.WriteLine($"в этом числе нет 3й цифры");
+}
 
+}
 
-    
+void Task10_2() // переворот трехзначного числа 
+{        
+            // Напишите программу, которая принимает на вход трёхзначное число 
+            // и на выходе показывает перевернутую цифру этого числа.
 
+        int number = InputNumber("Введите трехзначное число ");
+        //123 
+        int result = number % 10 * 100 + number / 10 % 10 * 10 + number / 100;
+  
+        Console.WriteLine($"{number}-> {result}");
+}
+Console.Clear();
+Task13();
